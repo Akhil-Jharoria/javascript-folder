47bd178b128c79c2ae43da79f5d1a2773e44a17b
Akhil Jharoria <95032356+Akhil-Jharoria@users.noreply.github.com>
2024-01-03 13:00:52 +0000

    about type conversion

---
+++ practicejs/typeconversion.js
@@ -0,0 +1,14 @@
+// here we learning about the type conversion of the data type in javascript.
+
+
+//console.log("hello friends");
+
+let age=78;
+ 
+  console.log(typeof age);
+
+  let stringage=String(age);
+    console.log("after conversion "+typeof stringage);
+
+
+
