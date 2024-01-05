27fa7495725fd2639583873c6b418fbb903410fc
Akhil Jharoria <95032356+Akhil-Jharoria@users.noreply.github.com>
2024-01-05 16:09:46 +0000

    memory-allocation

---
+++ practicejs/memory-allocation.js
@@ -0,0 +1,9 @@
+// here we see some memory allocation mechanism
+
+let myName='akhil jharoria';
+
+let anotherName='chotu';
+
+
+console.log(myName);
+console.log(anotherName);
---
+++ practicejs/returntype.js
@@ -0,0 +1,35 @@
+// here we see some of the return type of the data types
+
+    //premitivies data types
+    // let age=34;
+    // let name='Akhil';
+    // let boolValue=true;
+    // let nullValue=null;
+    // let undefinedValue;
+    // let bigIntValue=452645455677456767454464n;
+
+    // console.log(typeof age);
+    // console.log(typeof name);
+    // console.log(typeof boolValue);
+    // console.log(typeof nullValue);
+    // console.log(typeof undefinedValue);
+    // console.log(typeof bigIntValue);
+
+
+    // const fun=function myfun(){
+    //     console.log('hello world');
+    // }
+  
+    //  console.log(typeof fun);
+
+
+    // const obj={
+    //     name:'akhil',
+    //     age:21
+    // };
+
+    // console.log(typeof obj);
+
+    const arr=['akhil','ram','shyam',4,5.4,true];
+    console.log(typeof arr);
+    console.log(arr);
