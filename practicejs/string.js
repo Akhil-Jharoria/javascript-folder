29367747728e7da65e9e9aefd877d476f93f3a24
Akhil Jharoria <95032356+Akhil-Jharoria@users.noreply.github.com>
2024-01-10 08:35:06 +0000

    string method

---
+++ practicejs/string.js
@@ -0,0 +1,68 @@
+// here we are learning about the string
+
+
+// // first way
+// let str="AKHIL";
+// //second way
+// const str1='akhil';
+// //third way
+// let str2= new String('chotu');
+// console.log(typeof str);
+// console.log(typeof  str1);
+// console.log(typeof str2);
+
+// let str="hello how are you";
+
+// for(let i=0;i<str.length;i++){
+//     console.log(str[i]);
+// }
+
+// console.log("the value is "+str[5]+" hello");
+
+//templete literals(string intepolation)
+
+// let user='Akhil Jharoria';
+// let str=`Hello ${user} ! How are you.`;
+// console.log(str);
+
+let str=`this is my first program is of my class`;
+console.log(str);
+// console.log(typeof str);
+// some popular method on the string
+//length
+  //console.log(str.length);
+// char at
+// console.log(str.charAt());
+//slice
+ //console.log(str.slice(-4,9));
+ //substring
+//  console.log(str.substring(-5,9));
+
+//concat
+// let str2="I am fine";
+//  console.log(str.concat(str2));
+//reapeat
+//console.log(str.repeat(4));
+
+//replace
+// let str1='please visit microsoft';
+ // let newstr=str1.replace('Microsoft','home');
+ //console.log(newstr);
+
+//  let str1="this-is-my-second-program"
+// console.log(typeof str);
+// let newarr=str1.split('-');
+// console.log(newarr);
+// console.log(typeof newarr);
+// console.log(Array.isArray(newarr));
+
+
+// //indexof
+// console.log(str.indexOf('my'));
+
+// console.log((str.includes('Is')));
+
+
+//.search
+
+console.log(str.search('my'));
