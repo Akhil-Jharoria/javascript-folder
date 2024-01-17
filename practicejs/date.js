d58e563ef1356835db97ab6d238cb83179b0c015
Akhil Jharoria <95032356+Akhil-Jharoria@users.noreply.github.com>
2024-01-17 17:57:45 +0000

    about date and time

---
+++ practicejs/date.js
@@ -0,0 +1,83 @@
+//Here we are learning about the date object in the javascript
+
+//const dt=new Date();
+//  console.log(dt);
+
+// const dt=new Date(2024,2,14);
+// console.log(dt);
+
+// const dt=new Date();
+// console.log(dt);
+// let strDate=dt.toString();
+// console.log(strDate);
+
+
+// const dt=new Date();
+// console.log(dt);
+// let strDate=dt.toTimeString();
+// console.log(strDate);
+
+
+// const dt=new Date();
+// console.log(dt);
+// let strDate=dt.toDateString();
+// console.log(strDate);
+
+// const dt=new Date();
+// console.log(dt);
+// let strDate=dt.toLocaleDateString();
+// console.log(strDate);
+
+// const dt=new Date();
+// console.log(dt);
+// let strDate=dt.toLocaleString();
+// console.log(strDate);
+
+// const dt=new Date();
+// console.log(dt);
+// let strDate=dt.toLocaleTimeString();
+// console.log(strDate);
+
+// const dt=new Date();
+// console.log(dt);
+// let strDate=dt.toUTCString();
+// console.log(strDate);
+
+// const dt=new Date();
+// console.log(dt);
+// let strDate=dt.toISOString();
+// console.log(strDate);
+
+// const dt=new Date();
+// console.log(dt);
+// let strDate=dt.toJSON();
+// console.log(strDate);
+
+// const dt=new Date();
+// console.log(dt);
+// let strDate=dt.getFullYear();
+// console.log(strDate);
+
+// const dt=new Date();
+// console.log(dt);
+// let strDate=dt.getDay();
+// console.log(strDate);
+
+const d1= new Date();
+const d2= new Date(2024,0,12);
+
+if(d1>d2){
+    console.log("correct");
+    console.log("d1",d1);
+    console.log('d2',d2);
+}
+else {
+    console.log("incorrect");
+    console.log("d1",d1);
+    console.log('d2',d2);
+}
+  
+
+
+//and there are many more methods to learn;
+
