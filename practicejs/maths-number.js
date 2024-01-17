9fc89dbdf75feb51c61b278a93c1e0044e18428b
Akhil Jharoria <95032356+Akhil-Jharoria@users.noreply.github.com>
2024-01-17 10:12:51 +0000

    maths and number

---
+++ practicejs/maths-number.js
@@ -0,0 +1,59 @@
+//Hello guys!. Here we are learning about the Math library and Number in the 
+//javascript
+
+//math object allow to perform mathematical operation in the javascript
+
+
+let myNum;
+//console.log("the number is",myNum);
+myNum=Math.PI;
+// console.log("the number is",myNum);
+
+myNum=-48957;
+mynum2=-5236773;
+mynum3=-4723;
+//console.log(Math.abs(myNum));
+// console.log(Math.cbrt(8));
+// console.log(Math.ceil(8.17));
+// console.log(Math.floor(8.934));
+//console.log(Math.round(8.1834));
+const arr=[8,2,6,9,10]
+//console.log(Math.max(...arr));
+//console.log(Math.min(arr));
+//console.log(Math.pow(2,3));
+//console.log(Math.sqrt(4));  
+//console.log(Math.round(Math.random()*10));
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Number
+
+// let x=3.14;
+// let y= new Number(45);
+// console.log(y);
+// console.log(x);
+
+// let x=123e5;
+// console.log(x);
+
+// let x=345678;
+// console.log(x.toString());
+// console.log(typeof x);
+
+// let x=300;
+// let z=String(x);
+// console.log(z);
+// console.log(typeof z);
+
+
+let x=39456001;
+let numStr=x.toString();
+
+console.log(x);
+console.log("the numbe is",typeof x);
+console.log("The string is",typeof numStr);
+console.log(numStr);
+
+
+
+
+
