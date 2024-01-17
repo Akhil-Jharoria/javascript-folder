83a64fb3b6d2242cec8268a21707726b59084e79
Akhil Jharoria <95032356+Akhil-Jharoria@users.noreply.github.com>
2024-01-17 18:25:33 +0000

    about array

---
+++ practicejs/array.js
@@ -0,0 +1,35 @@
+// Here we are going to learn about the array in the javascript.
+// array is object in js
+
+// const arr=new Array(4);
+// arr[0]='hello';
+// arr[1]=false;
+// arr[2]=23;
+// arr[3]=23.54;
+// console.log(arr);
+// console.log("the type of array is",typeof arr);
+
+let arr1= [1,2,'akhil',false,47.34];
+let arr2=arr1;
+
+// console.log(arr1);
+// console.log(arr2);
+
+// arr2[3]="jharoria";
+
+// console.log(arr1);
+// console.log(arr2);
+
+// arr1.delete;
+// console.log(arr1);
+// console.log(arr2);
+
+
+// let str=arr1.toString();
+// console.log(str);
+
+
+// let str=arr1.toLocaleString();
+// console.log(str);
+
+console.log(Array.isArray(2));
