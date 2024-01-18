e6db3cfddc1362d3c55dc60592a705cae19deb6a
Akhil Jharoria <95032356+Akhil-Jharoria@users.noreply.github.com>
2024-01-18 11:35:01 +0000

    some more of scope

--- practicejs/test.js
+++ practicejs/test.js
@@ -1,1 +1,18 @@
 console.log("hello");
+
+let sum=1234;
+function myfun(){
+    let sumInside=435;
+    console.log("inside function",sum);
+
+    function myfun2(){
+        console.log(sumInside);
+        let sumInside2=5678;
+    }
+
+    //console.log(sumInside2);
+    myfun2();
+}
+//console.log(sumInside);
+myfun();
+console.log('outside function ',sum);
