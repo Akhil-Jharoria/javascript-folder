82f621e0f41bb32ad93c0cf5fdded5cd13ff6071
Akhil Jharoria <95032356+Akhil-Jharoria@users.noreply.github.com>
2024-01-18 15:19:17 +0000

    about this keyword

---
+++ practicejs/this.js
@@ -0,0 +1,25 @@
+// Here we are learning about this keyword in javascript
+//This is very power full keyword in js.
+
+// console.log(this);
+
+// const user={
+//     name:'Akhil',
+//     age:21,
+//     email:'akhiljharoria7898@microsoft.com',
+//     greeting: function(params) {
+//         console.log(`Hello ${this.name}. Welcome to our website.`);
+//     }
+// };
+
+// console.log(user.greeting());
+
+// console.log(this);
+
+// function myfun() {
+
+//     let username='akhil';
+//     console.log(this.username);      will not work
+// }
+
+myfun();
