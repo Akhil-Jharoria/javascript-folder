5c976ed0d1b289d80cc68cf610a2c38cdb15b58f
Akhil Jharoria <95032356+Akhil-Jharoria@users.noreply.github.com>
2024-01-18 08:36:48 +0000

    about arrray and its methods

--- practicejs/array.js
+++ practicejs/array.js
@@ -10,26 +10,80 @@
 // console.log("the type of array is",typeof arr);
 
 let arr1= [1,2,'akhil',false,47.34];
-let arr2=arr1;
+// let arr1={1,2:'akhil',false,45.67};
+// let arr2=arr1;
 
 // console.log(arr1);
 // console.log(arr2);
-
 // arr2[3]="jharoria";
-
 // console.log(arr1);
 // console.log(arr2);
-
 // arr1.delete;
 // console.log(arr1);
 // console.log(arr2);
-
-
 // let str=arr1.toString();
 // console.log(str);
-
-
 // let str=arr1.toLocaleString();
 // console.log(str);
+// console.log(Array.isArray(2));
 
-console.log(Array.isArray(2));
+// console.log(arr1);
+//console.log(arr1.length);
+//console.log(arr1.toString());
+// let newarr=arr1.join();
+// console.log(newarr);
+
+// arr1.push('hello ji sb');
+// console.log(arr1);
+
+// arr1.pop();
+// console.log(arr1);
+
+// arr1.shift();
+// console.log(arr1);
+
+// arr1.unshift('hello darling');
+// console.log(arr1);
+
+// let arr2=[9,8,7,6,5];
+// console.log(arr2);
+
+// console.log(arr1.concat(arr2));
+// console.log(arr1);
+// console.log(arr2);
+
+// let remvArr=arr1.splice(2,2,'A','B','C');
+// console.log(arr1);
+// console.log(remvArr);
+
+
+// let remvArr=arr1.slice(2,4);
+// console.log(remvArr);
+// console.log(arr1);
+
+// let arr2=[
+//     {name:'akhil'},
+//     function myname()
+//     { console.log('hello every one');}];
+// console.log(arr2[0].name);
+// arr2[1]();
+
+// let arr2=[3,6,[4,2,1,[35,89,23,[4545,454,23,3]]]];
+// console.log(arr2);
+// console.log(arr2.flat(Infinity));
+
+// let arr2=[2,3,4];
+// let arr3=[5,6,7];
+// let arr4=[8,7,6];
+
+// let newarr=[...arr1,...arr2,...arr3,...arr4];
+// console.log(newarr);
+// console.log(arr1.includes(false));
+
+// let arr2=['A','B','C','D','E','F'];
+// let arr2=[1,7,5,3,5,1,2];
+// console.log(arr2.sort());
+
+
+
+
