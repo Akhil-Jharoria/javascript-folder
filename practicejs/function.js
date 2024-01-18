e5a220290a1a9ecc18c6771f448f9b238273408a
Akhil Jharoria <95032356+Akhil-Jharoria@users.noreply.github.com>
2024-01-18 10:54:09 +0000

    about function

---
+++ practicejs/function.js
@@ -0,0 +1,30 @@
+//Here we are about fucntion and more about function
+
+// function myfun(){
+//     console.log('Hello World');
+//     return 'return by function';
+// }
+
+// console.log(myfun);
+// let fun=myfun();
+// console.log(fun);
+
+
+//syntax
+
+  function functionname(par,para){
+    //body of the function 
+  }
+
+  //functionname(arguments)                  //function calls
+
+  let num=2;
+
+  function square(num){
+    return num*num;
+  }
+
+  console.log(num);
+  console.log(square(3));
+  
+  
