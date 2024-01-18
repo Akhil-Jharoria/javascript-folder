316c80e4958eca5df0a41a75a01a090b3d40d997
Akhil Jharoria <95032356+Akhil-Jharoria@users.noreply.github.com>
2024-01-18 15:48:24 +0000

    about IIFE

---
+++ practicejs/iife.js
@@ -0,0 +1,18 @@
+//Here we are learning about the IIFE(Immediately Invoke function expression)
+
+
+
+// (function myfun(){                   named iife
+//     console.log("hello inside IIFE");
+// })();
+
+// ((username)=>{                       un named IIFE
+//     console.log('inside the arrow function',username);
+// })('akhil');
+
+
+(function (){                       //unnamed IIFE
+    console.log('hello ji kise ho saare');
+})();
+
+
