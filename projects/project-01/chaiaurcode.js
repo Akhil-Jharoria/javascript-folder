64cc11b94d08a083f6d8175885e5fe9da3c2d409
Akhil Jharoria <95032356+Akhil-Jharoria@users.noreply.github.com>
2024-01-22 12:12:35 +0000

    work done

---
+++ projects/project-01/chaiaurcode.js
@@ -0,0 +1,13 @@
+// here we write the javascript code for the project one
+
+const button=document.querySelectorAll('.button');
+console.log(button);
+
+// button.forEach((elm,index,button)=>{
+//    console.log(elm);
+//    console.log("this id is",elm.id);
+// });
+
+function myfunction(buttonid){
+    console.log('The clicked button is',buttonid);
+}
