47a700aa461334c6e37a592220f9420f6bf5c17e
Akhil Jharoria <95032356+Akhil-Jharoria@users.noreply.github.com>
2024-01-24 09:02:49 +0000

    project one complete

--- projects/project-01/chaiaurcode.js
+++ projects/project-01/chaiaurcode.js
@@ -1,13 +1,22 @@
 // here we write the javascript code for the project one
 
 const button=document.querySelectorAll('.button');
-console.log(button);
+const body= document.querySelector('body')
+//console.log(button);
 
-// button.forEach((elm,index,button)=>{
-//    console.log(elm);
-//    console.log("this id is",elm.id);
-// });
+// function myfunction(buttonid){
+//     console.log('The clicked button is',buttonid);
+//     body.style.backgroundColor=`${buttonid}`;
+// }
 
-function myfunction(buttonid){
-    console.log('The clicked button is',buttonid);
-}
+button.forEach(function (button){
+  console.log(button);
+
+    button.addEventListener('click', function (event){
+        //console.log(event);
+        //console.log(event.target);
+
+        console.log(event.target.id);
+        body.style.backgroundColor=event.target.id;
+    })
+});
