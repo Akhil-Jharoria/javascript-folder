dd3bf33d0efe9f0fab09bb8cde738c47d0c1b9af
Akhil Jharoria <95032356+Akhil-Jharoria@users.noreply.github.com>
2024-02-07 10:38:50 +0000

    task to complete

---
+++ projects/task/script.js
@@ -0,0 +1,23 @@
+//Code for task 1
+// const txt=document.querySelector('#txt');
+// const butt=document.querySelector('#clickme')
+// //console.log(txt);
+// //console.log(butt);
+
+// butt.addEventListener('click',(obj)=>{
+//     txt.style.display='none';
+// });
+
+
+
+//code for task two
+
+const field=document.querySelector('#field');
+//console.log(field);
+
+field.addEventListener('click',(obj)=>{
+
+     console.log(obj.ClientX);
+     //console.log(obj.ClientY);
+     
+});
