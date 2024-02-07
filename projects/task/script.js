2326a362d4ddc2f0cd7f125ceaf48e84fd982065
Akhil Jharoria <95032356+Akhil-Jharoria@users.noreply.github.com>
2024-02-07 12:35:19 +0000

    task completed

--- projects/task/script.js
+++ projects/task/script.js
@@ -12,12 +12,58 @@
 
 //code for task two
 
-const field=document.querySelector('#field');
-//console.log(field);
+// const field=document.querySelector('#field');
+// //console.log(field);
 
-field.addEventListener('click',(obj)=>{
+// field.addEventListener('click',(obj)=>{
 
-     console.log(obj.ClientX);
-     //console.log(obj.ClientY);
+//      let fieldCoords = field.getBoundingClientRect();
+//      let ballCoords = {
+//           top: obj.clientY - fieldCoords.top - field.clientTop - ball.clientHeight / 2,
+//           left: obj.clientX - fieldCoords.left - field.clientLeft - ball.clientWidth / 2
+//         };
+
+
+//    // prevent crossing the top field boundary
+//    if (ballCoords.top < 0) ballCoords.top = 0;
+
+//    // prevent crossing the left field boundary
+//    if (ballCoords.left < 0) ballCoords.left = 0;
      
+//       // // prevent crossing the right field boundary
+//       if (ballCoords.left + ball.clientWidth > field.clientWidth) {
+//           ballCoords.left = field.clientWidth - ball.clientWidth;
+//         }
+     
+//         // prevent crossing the bottom field boundary
+//         if (ballCoords.top + ball.clientHeight > field.clientHeight) {
+//           ballCoords.top = field.clientHeight - ball.clientHeight;
+//         }
+
+//         ball.style.left = ballCoords.left + 'px';
+//         ball.style.top = ballCoords.top + 'px';
+// });
+
+
+
+//code for task three;
+
+
+let toogle=true;
+
+const list=document.querySelector('#list');
+const title=document.querySelector('.title');
+// const menu=document.querySelector('.menu');
+const menu=document.querySelector('#Sweeties');
+
+title.addEventListener('click',()=>{
+
+       toogle=!toogle;
+
+     if(toogle){
+          list.remove();
+     }
+     else{
+          menu.appendChild(list)
+     }
 });
