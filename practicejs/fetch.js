a40b3d4f78d2e5b52690e0a5ca24e80d011973e1
Akhil Jharoria <95032356+Akhil-Jharoria@users.noreply.github.com>
2024-02-10 08:39:14 +0000

    fetch api completed

--- practicejs/fetch.js
+++ practicejs/fetch.js
@@ -1,28 +1,36 @@
 //Here we are learning about the fetch api
-
-
-// const fetchRep=fetch('https://www.edamam.com/');
-// fetchRep.then((response)=>{
-//   //   console.log(response);
-//      const data= response.json();
-//      console.log(data);
-// }).catch((error)=>{
-//     console.log(error);
-// })
-
-async function getdata(){
-
-    try {
-
-       // const response= await fetch('https://api.github.com/users/Akhil-Jharoria');
-        const response= await fetch('https://fakestoreapi.com/');
-       // console.log(response);
-        //console.log(typeof response);
-          const data= await response.json();
-          console.log(data);
-    } catch (error) {
-         console.log(error);
-    }
+function myfunction(resp){
+      console.log(resp);
+      console.log('Inside my function ');
 }
 
-getdata();
+const fetchRep=fetch('https://fakestoreapi.com/products/');
+fetchRep.then((response)=>{
+  if(response.ok){
+    const data= response.json();
+    return data;
+  }
+})
+.then((data)=>{
+   
+      console.log('the data is recieved successfully');
+      //console.log(data);
+      myfunction(data);
+})
+.catch((error)=>{
+    console.log(error);
+})
+
+// async function getdata(){
+//     try {
+//        // const response= await fetch('https://api.github.com/users/Akhil-Jharoria');
+//         const response= await fetch('https://fakestoreapi.com/products/');
+//        // console.log(response);
+//         //console.log(typeof response);
+//           const data= await response.json();
+//           console.log(data);
+//     } catch (error) {
+//          console.log(error);
+//     }
+// }
+// getdata();
